3d717109cbd7c585ec3a436fc151a740cb3d0254
Leta-D <letadeju.1278@gmail.com>
2025-08-18 16:43:53 +0300

    fixed gradle issues

--- mobile_app/kind_bridge/android/build.gradle.kts
+++ mobile_app/kind_bridge/android/build.gradle.kts
@@ -1,13 +1,10 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("com.google.gms:google-services:4.4.3")
-    }
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false
 }
+
 
 allprojects {
     repositories {
@@ -23,9 +20,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
